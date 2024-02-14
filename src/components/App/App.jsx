0d86d21c9daa1a78b5a8ea538f2b50029c94f8ea
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-14 16:10:32 -0500

    created Header component and imported into App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
@@ -53,9 +55,7 @@ function App() {
   console.log(newGuestMeal)
   return (
     <div className="App">
-      <header>
-        <h1>Prim Proper Props</h1>
-      </header>
+      <Header />
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
       <h2>Add a new guest</h2>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+ 
+  return (
+   
+      <header>
+        <h1>Prim Proper Props</h1>
+      </header>
+  );
+}
+
+export default Header;
