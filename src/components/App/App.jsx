fa06a63b38664a6a9edf7232fb585ac9bb155696
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-14 21:22:53 -0500

    created Footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
 import './App.css';
 
 function App() {
@@ -127,10 +129,7 @@ function App() {
       <div>
         Knives: {guestList.length * 2}
       </div>
-      <footer>
-        <h3>Have fun!</h3>
-        <p>Don't forget to mind your Ps and Qs!</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer() {
+ 
+    return (
+     <div>
+        <h3>Have fun!</h3>
+        <p>Don't forget to mind your Ps and Qs!</p>
+     </div>
+    );
+  }
+  
+  export default Footer;
+  
